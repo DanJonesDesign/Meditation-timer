69767780223b8297b64b4dc32e95499ea2d168f5
DanJonesDesign <djproductdesign@gmail.com>
2025-01-05 11:55:31 +0000

    Update slider.jsx

--- src/components/ui/slider.jsx
+++ src/components/ui/slider.jsx
@@ -1,30 +1,16 @@
-import * as React from "react"
-
-const Slider = React.forwardRef(({ value = [0], min = 0, max = 100, step = 1, onValueChange, ...props }, ref) => {
-  const percentage = ((value[0] - min) / (max - min)) * 100
-  
-  const handleChange = (e) => {
-    const newValue = Number(e.target.value)
-    onValueChange([newValue])
-  }
-  
+export function Slider({ value = [0], min = 0, max = 100, step = 1, onValueChange, className, ...props }) {
   return (
-    <div className="relative w-full h-6 flex items-center" ref={ref} {...props}>
+    <div className="relative flex w-full touch-none select-none items-center">
       <input
         type="range"
         min={min}
         max={max}
         step={step}
         value={value[0]}
-        onChange={handleChange}
-        className="w-full h-2 bg-gray-200 rounded-full appearance-none cursor-pointer"
-        style={{
-          background: `linear-gradient(to right, rgb(59 130 246) ${percentage}%, rgb(229 231 235) ${percentage}%)`
-        }}
+        onChange={(e) => onValueChange([Number(e.target.value)])}
+        className={`h-2 w-full rounded-full bg-gray-200 ${className}`}
+        {...props}
       />
     </div>
   )
-})
-Slider.displayName = "Slider"
-
-export { Slider }
+}
