320f1c4d4e56de2f4b5a971bfa7d3977237641e5
DanJonesDesign <djproductdesign@gmail.com>
2025-01-05 12:21:28 +0000

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import MeditationTimer from '../../components/ui/MeditationTimer';
+import MeditationTimer from '../components/ui/MeditationTimer';
 
 export default function Home() {
   return (
