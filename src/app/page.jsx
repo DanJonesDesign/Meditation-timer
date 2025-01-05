78bd5c1ac338adb45dd6d5c9a49cefba367300da
DanJonesDesign <djproductdesign@gmail.com>
2025-01-05 12:14:57 +0000

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,7 @@
+"use client";
+
+import MeditationTimer from '@/components/ui/MeditationTimer';
+
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center p-4 bg-gray-50">
